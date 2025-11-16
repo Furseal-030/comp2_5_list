4d5838865cbe0c9f3e231988ba4d37ff9ede55fd
Furseal-030 <sheteriayong@gmail.com>
2025-11-16 21:45:21 +0900

    テストを通るようにしました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,14 +1,15 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
 
+
 #include "../include/lib_func.h"
 
 
 // ノードを初期化
 void initialize_node(node* p, int val)
 {
-	p->pNext = NULL;
-	p->data = val;
+	p->pNext = NULL; //ポインタ	
+	p->data = val;  //データ
 }
 
 // リストを初期化
@@ -22,12 +23,20 @@ void initialize_list(list* l)
 void push_front(list* l, node* p)
 {
 	// ToDo: ここにコードを追加
+
+	p->pNext = l->header;  //次を元々の先頭にする
+	l->header = p;  //先頭を更新
+
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
 	// ToDo: ここにコードを追加
+
+	if (!p->pNext) return;  //次のが存在してなかったらやめる
+	node* target = p->pNext;  //pNextをtargetに保存
+	p->pNext = target->pNext;  //pからに繋ぎ変え
 }
 
 
